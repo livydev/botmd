d2025d4dc39cb6f8d344e98cefcf7a222f6dafe7
AxellXd <101438987+AxellXyz@users.noreply.github.com>
2022-04-16 06:12:07 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,7 +14,7 @@ const q = {
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6288217843188', 'Reza Temayang', m)
+conn.sendContact(m.chat, '6288289338073', 'Axell Punya Selly', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
